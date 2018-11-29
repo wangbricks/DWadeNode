cda1688e5c4c11c79f24c170846b6c02671c8a72
bricks <brickswong@163.com>
2018-11-29 23:31:37 +0800

    expres 大家家口服务器  未连接sql

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 var express = require("express")
+var app = express()
+var bodyParser = require("body-parser")
+app.use(bodyParser.json() )
+app.use(bodyParser.urlencoded({extended:true}))
+
+//跨域
+app.all("*",function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+})
+app.get("/default",function(req,res){
+    let data={
+        name:"bricks",
+        age:26
+    }
+    res.status(200)
+    res.json(data)
+})
+app.post("/user",function(req,res){
+    console.log(req.stack);
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
+    let data={
+        name:"bricks",
+        age:26
+    }
+    res.json(data)
+})
+var server = app.listen(6060, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+})
